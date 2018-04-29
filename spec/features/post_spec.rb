f591f8a04c50fb54f2b2e78fe6ee92a720e039b6
Ahamed <ahamedali95@gmail.com>
2018-04-29 14:46:07 -0400

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 describe 'new post' do
+  it "ensures that that the form route works with the new action" do
+    visit new_post_path
+    expect(page.status_code).to equal(200)
+  end
 
+  it "renders the html file containing the form" do
+    visit new_post_path
+    expect(page).to have_content("Post Form")
+  end
+
+  it "submission will redirect the user to the show page where it displays the title and description of the post" do
+    visit new_post_path
+    fill_in "post_title", with: "My post title"
+    fill_in "post_description", with: "My post description"
+
+    click_on "Submit Post"
+
+    expect(page.current_path).to eq(posts_path)
+    expect(page).to have_content('My post title')
+    expect(page).to have_content('My post description')
+  end
 end
